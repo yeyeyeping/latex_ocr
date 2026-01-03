93bf7307d1ebe63bb96741e0daa1eadd39e9080b
yeyeyeping <82635195@qq.com>
2026-01-03 14:44:56 +0800

    change sft prompt

--- args.py
+++ args.py
@@ -22,26 +22,7 @@ class ModelArgument:
 class DataArgument:
     data_folder = "/root/project/code/data/data/intern"
     tgt_image_size: int = 448
-    prompt: str = """你是一位专业的 LaTeX 排版专家，擅长从数学公式图像中精准还原高质量的 LaTeX 代码.请根据图片中的公式生成对应的 latex 公式文本：
-                    输出格式要求：
-                    1. 必须使用 ```latex 代码块包裹
-                    2. 仅包含 LaTeX 代码，无任何文字说明
-                    3. 确保语法正确，可以通过编译，所有下标用 {} 括起来
-
-                    输出案例：
-                    案例 1：
-                    ```latex
-                    \sum_{i=1}^{n} x_i = \mu
-                    ```
-
-                    案例 2：
-                    ```latex
-                    \begin{bmatrix}
-                    a & b \\
-                    c & d
-                    \end{bmatrix}
-                    ```
-                    """
+    prompt: str = """请从输入的图像中还原LaTeX 代码,保证输出的latex代码语法正确："""
 
 
 def parse_args()->Tuple[TrainingArguments,ModelArgument,DataArgument]:
